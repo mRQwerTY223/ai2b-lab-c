246c0a09e311e462c2bad650d41bf4828622b3fd
Marek Zydor <zm49458@zut.edu.pl>
2023-11-28 18:09:06 +0100

    lab-c

---
+++ src/app/add-person/add-person.component.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+import {Person} from "../person";
+import {PersonLsService} from "../person-ls.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-add-person',
+  templateUrl: './add-person.component.html',
+  styleUrls: ['./add-person.component.css']
+})
+export class AddPersonComponent{
+  person: Person = {
+    address: {},
+  }
+
+  constructor(
+    private personLsService: PersonLsService,
+    private router: Router,
+  ) {
+  }
+
+  save() {
+    this.personLsService.addPerson(this.person);
+    this.router.navigate(['/list']);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterOutlet} from "@angular/router";
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { AddPersonComponent } from './add-person/add-person.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailsComponent,
+    AddPersonComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
